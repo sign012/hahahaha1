6b734a9116f1a85eb3273d990d7d63e0be3052ca
Sham <9141313+Afang_web@user.noreply.gitee.com>
2021-08-10 20:47:13 +0800

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <el-button type="primary" @click="getInfo">爱你</el-button>
+    <el-button type="success">爱你</el-button>
+    <el-button type="info">爱你</el-button>
+    <el-button type="warning">爱你</el-button>
+    <el-button type="danger">爱你</el-button>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    async getInfo() {
+      // this.$http.get() 获得的是一个promise
+      //getInfo前面加修饰符async Promise前面加await 就变成一个对象
+      // 利用解构法 获取data: res
+     const { data: res } = await this.$http.get('/api/get', { 
+        params: {name: 'zs', age: 20}
+      })
+      console.log(res);
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './element-ui'
 
+import axios from 'axios'
+axios.defaults.baseURL = 'https://www.escook.cn'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
